2a88f9dfa96a44adf3980bcbf9c7772e65fdb3b7
stefanoquer <stefano.quer@polito.it>
2025-04-15 11:18:27 +0200

    StQ

--- Generator/main.cpp
+++ Generator/main.cpp
@@ -1,15 +1,24 @@
 /*
- *  This work is lecende mnder
- *  CC BY-NC-ND 4.0 Deed
- *  Attribution-NonCommercial-NoDerivs 4.0 International 
- *  Canonical URL https://creativecommons.org/licenses/by-nc-nd/4.0/
+ * Copyright 2023 Stefano Quer
  *
- *  StQ 20.09.2023
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see https://www.gnu.org/licenses/
+ */
+
+/*
  *  Board Generator
  *  - No parameters
  *  - It creates ../../Flying-Probe-Board/dir-board-with-date-of-today
- *
- *
  */
 
 #include <iostream>
